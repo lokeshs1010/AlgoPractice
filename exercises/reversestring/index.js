b9dbac7c2ae4a707c285d5792788ef4f702ea51b
lokeshs1010 <lokesh1010saini@gmail.com>
2020-02-14 01:13:05 +0530

    test: 💍 reverse string solution 1

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,12 +7,10 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  const strArr = str.split("");
-  let newArr = [];
-  strArr.forEach(element => {
-    newArr.unshift(element);
-  });
-  return newArr.join("");
+  return str
+    .split("")
+    .reverse()
+    .join("");
 }
 
 module.exports = reverse;
