7117d10b727320c907c3a0f20e77440cc3a8c64c
lokeshs1010 <lokesh1010saini@gmail.com>
2020-02-15 12:34:08 +0530

    test: 💍 palindeome solution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,17 +8,18 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-  let reversed = "";
-
-  for (let i of str) {
-    reversed = i + reversed;
-  }
-
-  if (str === reversed) {
-    return true;
-  }
-
-  return false;
+  return str.split("").every((char, index) => {
+    return char === str[str.length - index - 1];
+  });
 }
 
 module.exports = palindrome;
+
+// function palindrome(str) {
+//   const reversed = str
+//     .split("")
+//     .reverse()
+//     .join("");
+
+//   return str === reversed;
+// }
