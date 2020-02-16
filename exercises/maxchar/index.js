eba7e0fd9df1a233ed39351442ea6dd8c0bacea8
lokeshs1010 <lokesh1010saini@gmail.com>
2020-02-16 13:23:48 +0530

    test: 💍 max char my solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const arr = str.split("");
+  const obj = arr.reduce((acc, val) => {
+    if (acc[val]) {
+      acc[val] = acc[val] + 1;
+    } else {
+      acc[val] = 1;
+    }
+
+    return acc;
+  }, {});
+
+  let maxCharLength = 0;
+  let maxChar = "";
+  for (const [key, value] of Object.entries(obj)) {
+    if (maxCharLength < value) {
+      maxCharLength = value;
+      maxChar = key;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
