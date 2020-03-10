94dc4a832b094eb39ffea227477a1bbac0c6661c
lokeshs1010 <lokesh1010saini@gmail.com>
2020-03-10 10:36:45 +0530

    test: 💍 Chunk solution 2

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -10,14 +10,11 @@
 
 function chunk(array, size) {
   const chunked = [];
-  array.forEach(element => {
-    const last = chunked[chunked.length - 1];
-    if (!last || last.length === size) {
-      chunked.push([element]);
-    } else {
-      last.push(element);
-    }
-  });
+  let index = 0;
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
   return chunked;
 }
 
