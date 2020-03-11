c61701deef2564af5c9bfe1ddb7d80a38adb0c76
lokeshs1010 <lokesh1010saini@gmail.com>
2020-03-11 22:59:46 +0530

    test: 💍 Fibonaaci solution 1

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,11 +9,23 @@
 //   fib(4) === 3
 
 function fib(n) {
-  if (n === 0 || n === 1) {
-    return n;
+  const result = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    const a = result[i - 1];
+    const b = result[i - 2];
+    result.push(a + b);
   }
 
-  return fib(n - 1) + fib(n - 2);
+  return result[n];
 }
 
 module.exports = fib;
+
+// function fib(n) {
+//   if (n === 0 || n === 1) {
+//     return n;
+//   }
+
+//   return fib(n - 1) + fib(n - 2);
+// }
