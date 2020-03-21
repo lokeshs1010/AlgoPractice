f71692523548c580f0e2b02186e2160482d1ac7a
lokeshs1010 <lokesh1010saini@gmail.com>
2020-03-21 23:35:00 +0530

    test: 💍 Contains common item soluton 1

--- exercises/common/contains-common-items.js
+++ exercises/common/contains-common-items.js
@@ -1,7 +1,12 @@
 const containsCommonItems = (arr1, arr2) => {
+  const obj = {};
   let isIncluded = false;
-  arr1.forEach(element => {
-    if (arr2.includes(element)) {
+
+  arr1.forEach(elm => {
+    obj[elm] = true;
+  });
+  arr2.forEach(elm => {
+    if (obj[elm]) {
       isIncluded = true;
     }
   });
@@ -10,3 +15,13 @@ const containsCommonItems = (arr1, arr2) => {
 
 const result = containsCommonItems(["a", "b", "c", "x"], ["z", "y", "x"]);
 console.log(result);
+
+// const containsCommonItems = (arr1, arr2) => {
+//   let isIncluded = false;
+//   arr1.forEach(element => {
+//     if (arr2.includes(element)) {
+//       isIncluded = true;
+//     }
+//   });
+//   return isIncluded;
+// };
